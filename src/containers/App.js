fc5bd5037c0cc56b787c815958d4542ce9399b24
Suraj Vijay <surajcpp115@gmail.com>
2021-03-31 15:04:47 +0530

    Updated project with hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,44 +1,49 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import './App.css';
 
-class App extends React.Component{
-	constructor() {
-		super();
-		this.state = {
-			robots: [],
-			searchField: ""
-		}
-	}
+function App(){
+	// constructor() {
+	// 	super();
+	// 	this.state = {
+	// 		robots: [],
+	// 		searchField: ""
+	// 	}
+	// }
+	const [robots, setRobots] = useState([]);
+	const [searchField, setSearchField] = useState("");
+	// componentDidMount() {
+	// 	fetch("https://jsonplaceholder.typicode.com/users")
+	// 	.then(response => response.json())
+	// 	.then(users => this.setState({robots:users}));
+	// }
 
-	componentDidMount() {
+	//Alternate for componentDidMount()
+	useEffect(() => {
 		fetch("https://jsonplaceholder.typicode.com/users")
-		.then(response => response.json())
-		.then(users => this.setState({robots:users}));
-	}
+	 	.then(response => response.json())
+	 	.then(users => setRobots(users));
+	}, []);
 
-	onSearchChanged = (event) => {
-		this.setState({searchField: event.target.value})
+	const onSearchChanged = (event) => {
+		setSearchField(event.target.value);
 	}
+	
+	const filteredBots = robots.filter(robot => robot
+		.name.toLowerCase().includes(searchField.toLowerCase()));
 
-	render() {
-		const {robots, searchField} = this.state;
-		const filteredBots = robots.filter(robot => robot
-			.name.toLowerCase().includes(searchField.toLowerCase()));
-
-		return !robots.length ? <h1>Its Loading</h1> :
-			 (
-				<div className="tc">
-					<h1>RoboFriends</h1>
-					<SearchBox searchChange={this.onSearchChanged}/>
-					<Scroll>
-						<CardList robots={filteredBots}/>
-					</Scroll>
-				</div>
-			);
-	}
+	return !robots.length ? <h1>Its Loading</h1> :
+		 (
+			<div className="tc">
+				<h1>RoboFriends</h1>
+				<SearchBox searchChange={onSearchChanged}/>
+				<Scroll>
+					<CardList robots={filteredBots}/>
+				</Scroll>
+			</div>
+		);
 }
 
 export default App;
