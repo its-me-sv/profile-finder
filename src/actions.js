8e6101ed04acf37477413a22fa1769b5e77043cc
Suraj Vijay <surajcpp115@gmail.com>
2021-04-22 15:40:09 +0530

    catched error due to cors

--- src/actions.js
+++ src/actions.js
@@ -4,6 +4,7 @@ import {
 	REQUEST_ROBOTS_SUCCESS,
 	REQUEST_ROBOTS_FAILED
 } from './constants';
+import {robots} from './robots.js';
 
 export const setSearchField = text => ({
 	type: CHANGE_SEARCH_FIELD,
@@ -15,5 +16,5 @@ export const requestRobots = () => dispatch => {
 	fetch("https://jsonplaceholder.typicode.com/users")
 	.then(response => response.json())
 	.then(data => dispatch({type: REQUEST_ROBOTS_SUCCESS, payload: data}))
-	.catch(error => dispatch({type: REQUEST_ROBOTS_FAILED, payload: error}));
+	.catch(error => dispatch({type: REQUEST_ROBOTS_FAILED, payload: robots}));
 };
